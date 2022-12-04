2b9683a2eeb7b7da0a098b1905540d0530007c66
Stanley Wong <stanley.wong@axxes.com>
2022-12-04 23:21:45 +0100

    finished day 1

---
+++ Day1/Part1.js
@@ -0,0 +1,29 @@
+import { readFileSync } from "fs";
+const input = readFileSync("input.txt", "utf-8")
+  .toString()
+  .split(/\r\n\r\n/)
+  .map((x) => {
+    return x.split(/\r\n/).map((y) => {
+      return parseInt(y);
+    });
+  });
+const sumUpCalories = (calories) => {
+  return calories.map((x) => {
+    return x.reduce((a, b) => {
+      return a + b;
+    }, 0);
+  });
+};
+const part1 = (calories) => {
+  let sumOfCalories = sumUpCalories(calories);
+  console.log(`Part 1: \nMost calories: ${Math.max(...sumOfCalories)}`);
+};
+
+const part2 = (calories) => {
+  let sortedCalories = sumUpCalories(calories).sort((a, b) => b - a);
+  let sumTopThree = sortedCalories[0] + sortedCalories[1] + sortedCalories[2];
+  console.log(`Part 2: \nTotal calories top three: ${sumTopThree}`);
+};
+
+part1(input);
+part2(input);
